3766e24bda45fce639ee4424ef231e38761a8cf4
connor-ashcroft-1809 <connorjamesashcroft@gmail.com>
2023-12-06 20:44:36 +0000

    updated dag

--- dags/test_dag.py
+++ dags/test_dag.py
@@ -11,7 +11,7 @@ default_args = {
 
 
 with DAG(
-    dag_id ='our_first_dag_v5',
+    dag_id ='our_first_dag_v6',
     default_args = default_args,
     description='This is our first dag that we write',
     start_date=datetime(2023,12,2),
