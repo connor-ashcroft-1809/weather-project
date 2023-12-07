160ca16343c189c579de396489597b431b7637f9
connor-ashcroft-1809 <connorjamesashcroft@gmail.com>
2023-12-07 09:06:02 +0000

    commit postgres operator test

--- dags/test_dag.py
+++ dags/test_dag.py
@@ -1,36 +1,46 @@
-from datetime import datetime, timedelta
+from datetime import datetime,timedelta
+
 from airflow import DAG
-from airflow.operators.bash import BashOperator
+from airflow.providers.postgres.operators.postgres import PostgresOperator
 
 
 default_args = {
-    'owner' : 'connor',
+    'owner':'connor',
     'retries':5,
     'retry_delay':timedelta(minutes=2)
 }
 
-
 with DAG(
-    dag_id ='our_first_dag_v6',
-    default_args = default_args,
-    description='This is our first dag that we write',
-    start_date=datetime(2023,12,2),
-    schedule_interval='@daily'
+    default_args=default_args,
+    dag_id='dag_with_postgres_v3',
+    start_date=datetime(2023,12,3),
+    schedule_interval='0 0 * * *'
 ) as dag:
-    task1 = BashOperator(
-        task_id='first_task',
-        bash_command='echo hello world, this is the first task'
+    task1 = PostgresOperator(
+        task_id = 'create_postgres_table',
+        postgres_conn_id='postgres_conn',
+        sql="""
+            create table if not exists dag_runs(
+                dt date,
+                dag_id character varying,
+                primary key (dt, dag_id)
+            )
+        """
+    )
+    task2 = PostgresOperator(
+        task_id='delete_data_from_table',
+        postgres_conn_id='postgres_conn',
+        sql="""
+            delete from dag_runs where dt = '{{ ds }}' and dag_id ='{{ dag.dag_id }}'
+        """
     )
 
-    task2 = BashOperator(
-        task_id='second_task',
-        bash_command='echo hey, I am task2 and I will be running after task1'
+    task3 = PostgresOperator(
+        task_id='insert_into_table',
+        postgres_conn_id='postgres_conn',
+        sql="""
+            insert into dag_runs (dt, dag_id) values ('{{ ds }}', '{{ dag.dag_id }}')
+
+        """
     )
-    task3 = BashOperator(
-        task_id='third_task',
-        bash_command='echo hey, I am task3 and I will be running after task1, at the same time as task 2'
-
-    )
-
-
-    task1 >> [task2, task3]
+    task1 >> task2 >> task3
